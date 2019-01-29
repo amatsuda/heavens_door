41b95aca58940eb19342d807615f3af3aa1253c9
Akira Matsuda <ronnie@dio.jp>
2019-01-29 21:12:31 +0900

    Put it on the top-right

--- lib/heavens_door/engine.rb
+++ lib/heavens_door/engine.rb
@@ -6,6 +6,8 @@ module HeavensDoor
   class Engine < ::Rails::Engine #:nodoc:
     initializer 'HeavensDoor' do |app|
       app.middleware.use HeavensDoor::Middleware
+
+      app.config.assets.precompile += %w(heavens_door.css)
     end
   end
 end
